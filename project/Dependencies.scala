3b9f94e04d58e548307f1c6f7588f43aaff275be
Oscar Batori <oscarbatori@gmail.com>
2016-06-03 18:10:41 -0400

    Added spray.io dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,10 @@ object Dependencies {
     val kafkaVer = "0.9.0.1"
     val scoptVer = "3.4.0"
     val scalaHttpVer = "2.3.0"
-
+    val sprayJsonVer = "1.3.2"
+    val sprayVer = "1.3.1"
+    val jodaVer = "2.9.4"
+    val akkaVer = "2.3.15"
   }
 
   import Versions._
@@ -15,12 +18,18 @@ object Dependencies {
     "org.apache.kafka" % "kafka-clients" % kafkaVer
   )
 
-  val coreDeps = Seq(
-    "com.github.scopt" %% "scopt" % scoptVer,
-    "org.scalaj" %% "scalaj-http" % scalaHttpVer
+  val sprayDeps = Seq(
+    "io.spray" %% "spray-can" % sprayVer,
+    "io.spray" %% "spray-routing" % sprayVer,
+    "com.typesafe.akka" %% "akka-actor" % akkaVer
   )
 
-  val allDeps =
-    coreDeps ++
-      kafkaDeps
+  val coreDeps = Seq(
+    "com.github.scopt" %% "scopt" % scoptVer,
+    "org.scalaj" %% "scalaj-http" % scalaHttpVer,
+    "io.spray" %%  "spray-json" % sprayJsonVer,
+    "joda-time" % "joda-time" % jodaVer
+  )
+
+  val allDeps = coreDeps ++ kafkaDeps ++ sprayDeps
 }
